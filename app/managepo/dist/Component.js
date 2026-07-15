8980ef369c05334e358a56924b59148e020ff9bd
vibhuvarun <vibhuvarun27@gmail.com>
2026-07-15 11:05:30 +0000

    fixed xsuaa

---
+++ app/managepo/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("vibhu.po.managepo.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
